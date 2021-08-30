1bf97b80ff49c2d9479b16b46ce803ee796c9259
张琦 <qi.zhang@cyclone-robotics.com>
2021-08-30 14:49:44 +0800

    eject

--- config-overrides.js
+++ config-overrides.js
@@ -1,5 +1,6 @@
 const { name } = require('./package.json');
 console.log(name)
+const path = require('path')
 
 module.exports = {
   webpack: function override(config, env) {
@@ -10,6 +11,15 @@ module.exports = {
     config.output.library = `${name}-[name]`;
     config.output.libraryTarget = 'umd';
     config.output.jsonpFunction = `webpackJsonp_${name}`;
+    config.resolve.extensions = ['.js', '.json', '.jsx', '.ts', '.tsx'];
+    config.resolve.alias = {
+      "@src": path.resolve(__dirname, './src/'),
+      "@static": path.resolve(__dirname, './static/css/'),
+      "@libs": path.resolve(__dirname, './libs'),
+      "@store": path.resolve(__dirname, './store'),
+      "@components": path.resolve(__dirname, './components'),
+      "~": path.resolve(__dirname, './components')
+  }
     return config;
   },
   devServer: (configFunction) => {
